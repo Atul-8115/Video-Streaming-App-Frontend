65ce4156307e812c0b1bc82cbffe1eeeb52f89a0
Atul Pandey <pandeyatulkumar94@gmail.com>
2024-05-26 15:12:28 +0530

    feat: Add initial components for Feed, Navbar, SearchFeed, VideoDetails, and ChannelDetails
    This commit adds the initial components for the Feed, Navbar, SearchFeed, VideoDetails, and ChannelDetails. These components are essential for building the frontend of the video streaming app. They are currently empty and will be populated with content in future commits.

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Box } from '@mui/material'
+import Navbar from './components/Navbar'
+import Feed from './components/Feed'
+import VideoDetails from './components/VideoDetails'
+import ChannelDetails from './components/ChannelDetails'
+import SearchFeed from './components/SearchFeed'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Box sx={{backgroundColor: "#000"}}>
+        <Navbar/>
+        <Routes>
+            <Route path='/' exact element={<Feed/>}/>
+            <Route path='/video/:id' element={<VideoDetails/>}/>
+            <Route path='/channel/:id' element={<ChannelDetails/>}/>
+            <Route path='/search/:searchTerm' element={<SearchFeed/>}/>
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
 }
 
